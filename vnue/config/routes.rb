393b64a2d52c78149d0709477ba26fd801f26b7f
Sheldon Clark <sheldonclark@outlook.com>
2016-12-03 12:54:14 -0500

    fix postgres database names

--- vnue/config/routes.rb
+++ vnue/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'bands#index'
   resources :bands
+  resource :sessions, only: %i(new create destroy)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- vnue/db/schema.rb
+++ vnue/db/schema.rb
@@ -14,12 +14,61 @@ ActiveRecord::Schema.define(version: 20161202204518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "bands", force: :cascade do |t|
     t.string   "name"
     t.string   "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "date"
+    t.string   "time"
+    t.integer  "num_of_people"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.integer  "user_id"
+    t.text     "notes"
+    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id", using: :btree
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "location"
+    t.integer  "num_seats"
+    t.string   "name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "url"
+    t.string   "food_type"
+    t.string   "price_level"
+    t.string   "city"
+    t.integer  "current_seats"
+    t.float    "x_coord"
+    t.float    "y_coord"
+    t.string   "province"
+    t.string   "postal_code"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text    "review"
+    t.integer "users_id"
+    t.integer "restaurants_id"
+    t.string  "name"
+    t.index ["restaurants_id"], name: "index_reviews_on_restaurants_id", using: :btree
+    t.index ["users_id"], name: "index_reviews_on_users_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "fullname"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "phone_number"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
