692ae8a29f72ec37055846366b2eb27cb05c1c4a
Sheldon Clark <sheldonclark@outlook.com>
2016-12-03 11:54:02 -0500

    add bands routes

--- vnue/config/routes.rb
+++ vnue/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'bands#index.html.erb'
+  root 'bands#index'
+  resources :bands
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
