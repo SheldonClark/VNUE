0298033f943e532c358125b3231129a3891157da
Sheldon Clark <sheldonclark@outlook.com>
2016-12-06 14:31:35 -0500

    add ability to edit a band

--- vnue/app/controllers/bands_controller.rb
+++ vnue/app/controllers/bands_controller.rb
@@ -2,19 +2,31 @@ class BandsController < ApplicationController
 
   def new
   end
-  
+
   def index
     @bands = Band.all
   end
 
   def create
     @band = Band.new(band_params)
-
     if @band.save
       session[:band_id] = @band.id
       redirect_to root_url
     else
       render :new
+    end
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+  end
+
+  def update
+    @band = Band.find(params[:id])
+    if @band.update(params[:band].permit(:email, :password, :bio))
+      redirect_to @band
+    else
+      render 'edit'
     end
   end
 
