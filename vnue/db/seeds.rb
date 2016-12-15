22f1004e5857fb0fedaa6cb1b88a74e00d08f318
DAVID <davidnmwood@gmail.com>
2016-12-15 15:33:41 -0500

    add three Band test seed files, add one extra venue seed file

--- vnue/db/seeds.rb
+++ vnue/db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-    venue1 = Venue.create([{ name: 'cool venue', bio: 'great venue for bands to play at', email: 'coolvenue@gmail.com', phone_num: 1234567890 }])
 
+      #VENUE SEED FILES
+    venue1 = Venue.create([{ name: 'The Cavern', bio: 'Super fun times at King and Church', email: 'thecavern@gmail.com', phone_num: 1234567890 }])
+    venue2 = Venue.create([{ name: ' Sneaky Dees', bio: 'A great venue at College & Bathurst', email: 'sneakydees@gmail.com', phone_num: 1234567890 }])
+      #BAND SEED FILES
     band1 = Band.create([{ name: 'Sparkle Planet', bio: 'We play sparklecore music', email: 'sparkleplanet@gmail.com' }])
+    band2 = Band.create([{ name: 'Lords of Kitten', bio: 'We are really into metalcore, but what we mostly care about is kittens', email: 'lordsofkitten@gmail.com' }])
+    band3 = Band.create([{ name: 'Google Bears', bio: 'We wear bear costumes at our shows', email: 'googlebears@gmail.com' }])
